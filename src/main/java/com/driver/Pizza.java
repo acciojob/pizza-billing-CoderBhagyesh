de7bb1f0bdd20e082a6d23807d91fc5967cd18ac
Bhagyesh Khobragade <bhagyeshgk143@gmail.com>
2023-09-20 13:23:02 +0530

    Attempt 6

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,27 +1,37 @@
 package com.driver;
 
 public class Pizza {
-
     private int price;
     private Boolean isVeg;
     private String bill;
-    private Boolean extraCheese;
-    private Boolean extraTopping;
-    private Boolean takeAway;
-    private Boolean isBill = false;
+
+    private int cheese;
+    private int topping;
+    private int bag;
+    private int baseprice;
+    private boolean exchesse=false;
+    private boolean extopping=false;
+    private boolean exbag=false;
+
+    boolean billgenrated=false;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
         if(isVeg) {
-            this.price = 300;
-        } else {
-            this.price = 400;
+            this.price=300;
+            this.cheese=80;
+            this.topping=70;
+            this.baseprice=this.price;
+        }else {
+            this.price=400;
+            this.cheese=80;
+            this.topping=120;
+            this.baseprice=this.price;
         }
-        this.bill = "";
-        this.extraTopping = false;
-        this.extraCheese = false;
-        this.takeAway = false;
+        this.bag=20;
+
     }
 
     public int getPrice(){
@@ -30,44 +40,50 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        if (extraCheese==false) price += 80;
-        this.extraCheese = true;
+        if(exchesse==false) {
+            this.exchesse=true;
+            this.price+=this.cheese;
+
+        }
+
+
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(extraTopping==false) price += isVeg==true? 70:120;
-        this.extraTopping = true;
+        if(extopping ==false ) {
+            this.extopping=true;
+            this.price+=this.topping;
+
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
-        if(takeAway==false) price += 20;
-        this.takeAway = true;
+        if(exbag==false){
+            exbag=true;
+            this.price+=this.bag;
+        }
     }
 
     public String getBill(){
-        // your code goes
-        if(isBill==true) return "";
-
-        bill += "Base Price of the Pizza: ";
-        bill += isVeg? Integer.toString(300): Integer.toString(400);
-        bill += "\n";
-        bill += extraCheese?  "Extra Cheese Added: " + Integer.toString(80) : bill;
-        bill += "\n";
-        if(extraTopping) {
-            bill += "Extra Toppings Added: ";
-            if(isVeg) {
-                bill += Integer.toString(70);
-            } else {
-                bill += Integer.toString(120);
+        // your code goes here
+        if(billgenrated==false) {
+            billgenrated=true;
+            bill = "Base Price Of The Pizza: " + baseprice + "\n";
+            if (exchesse) {
+                bill += "Extra Cheese Added: " + cheese + "\n";
             }
+            if (extopping) {
+                bill += "Extra Toppings Added: " + topping + "\n";
+
+            }
+            if (exbag) {
+                bill += "Paperbag Added: " + bag + "\n";
+            }
+            bill += "Total Price: " + this.price+"\n";
         }
-        bill += "\n";
-        bill += takeAway? "Paperbag Added: " + Integer.toString(20): bill;
-        bill += "\n";
-        bill += "Total Price: " + Integer.toString(price);
-        isBill = true;
-        return bill;
+        return this.bill;
     }
 }
